40d2f83acd1786166e41e5ff0f29e84a78aff15a
dvetrov <vetrovs@ua.fm>
2015-04-09 18:07:37 +0300

    +

--- src/main/java/com/mkyong/common/config/AppSecurityConfig.java
+++ src/main/java/com/mkyong/common/config/AppSecurityConfig.java
@@ -8,12 +8,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("tom").password("123456").roles("USER");
+        auth.inMemoryAuthentication().withUser("vetrovs@ua.fm").password("322117").roles("USER");
         auth.inMemoryAuthentication().withUser("bill").password("123456").roles("ADMIN");
         auth.inMemoryAuthentication().withUser("james").password("123456").roles("SUPERADMIN");
     }
@@ -22,7 +21,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .antMatchers("/protected/**").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/login1").access("hasRole('ROLE_ADMIN')")
                 .antMatchers("/confidential/**").access("hasRole('ROLE_SUPERADMIN')")
                 .and().formLogin();
 
--- src/main/java/com/mkyong/common/config/RootConfig.java
+++ src/main/java/com/mkyong/common/config/RootConfig.java
@@ -4,9 +4,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
+@EnableWebSecurity
 @EnableScheduling
 @Configuration
 @Import({AppSecurityConfig.class, ViewConfig.class})
